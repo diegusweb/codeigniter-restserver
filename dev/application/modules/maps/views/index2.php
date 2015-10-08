bc0c65c78ee2cf6c5c3dbbb584c9bb448546d941
Diego Rueda <diego.rueda2@gmail.com>
2015-10-08 18:34:14 -0400

    add event for remove markers

--- dev/application/modules/maps/views/index2.php
+++ dev/application/modules/maps/views/index2.php
@@ -10,6 +10,8 @@
     var nameDIreccion = "";
     var latInfo = 0;
     var lngInfo = 0;
+    var markers = [];
+
 
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
@@ -51,10 +53,11 @@
     function placeMarker(location) {
         //alert(location);
 
-        var marker = new google.maps.Marker({
+       var marker = new google.maps.Marker({
             position: location,
             map: map
         });
+        markers.push(marker);
 
 
 
@@ -88,6 +91,31 @@
             error("Geocoding fallo debido a : " + status);
         }
     }
+    
+    // Sets the map on all markers in the array.
+        function setMapOnAll(map) {
+            console.log(markers.length);
+          for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+          }
+        }
+
+        // Removes the markers from the map, but keeps them in the array.
+        function clearMarkers() {
+          setMapOnAll(null);
+        }
+
+        // Shows any markers currently in the array.
+        function showMarkers() {
+          setMapOnAll(map);
+        }
+
+        // Deletes all markers in the array by removing references to them.
+        function deleteMarkers() {
+          clearMarkers();
+          markers = [];
+          direcciones = [];
+        }
 
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
@@ -98,20 +126,6 @@
 
 
 
-
-    /*for (i = 0; i < locations.length; i++) {  
-     marker = new google.maps.Marker({
-     position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-     map: map
-     });
-     
-     google.maps.event.addListener(marker, 'click', (function(marker, i) {
-     return function() {
-     infowindow.setContent(locations[i][0]);
-     infowindow.open(map, marker);
-     }
-     })(marker, i));
-     }*/
     $(document).ready(function () {
         $('.ver').click(function () {
             console.log(direcciones);
@@ -138,3 +152,8 @@
 
 Ruta<br>
 <button class="ver">guardar rutas</button>
+<br>
+      <input onclick="clearMarkers();" type=button value="Hide Markers">
+      <input onclick="showMarkers();" type=button value="Show All Markers">
+      <input onclick="deleteMarkers();" type=button value="Delete Markers">
+
